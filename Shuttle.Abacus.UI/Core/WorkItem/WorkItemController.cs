1aeb8542907a1bbd327c92e586d12115b556afb9
Eben <me@ebenroux.co.za>
2017-04-22 21:32:04 +0200

    -

--- Shuttle.Abacus.UI/Core/WorkItem/WorkItemController.cs
+++ Shuttle.Abacus.UI/Core/WorkItem/WorkItemController.cs
@@ -1,14 +1,20 @@
-using System;
 using Shuttle.Abacus.UI.Core.Messaging;
-using Shuttle.Abacus.UI.Messages.WorkItem;
+using Shuttle.Core.Infrastructure;
+using Shuttle.Esb;
 
 namespace Shuttle.Abacus.UI.Core.WorkItem
 {
     public abstract class WorkItemController : IWorkItemController
     {
-        public IBus Bus { get; set; }
-        public IWorkItem WorkItem { get; private set; }
-        public IMessageBus MessageBus { get; set; }
+        protected WorkItemController(IServiceBus serviceBus, IMessageBus messageBus)
+        {
+            ServiceBus = serviceBus;
+            MessageBus = messageBus;
+        }
+
+        protected IServiceBus ServiceBus { get; }
+        protected IMessageBus MessageBus { get; }
+        protected IWorkItem WorkItem { get; private set; }
 
         public void AssignWorkItem(IWorkItem workItem)
         {
@@ -17,14 +23,12 @@ namespace Shuttle.Abacus.UI.Core.WorkItem
             WorkItem = workItem;
         }
 
-        protected void Send(IMessage command)
+        public void Dispose()
         {
-            SetWorkItemWaiting();
-
-            Bus.Send(command).Register(ar => ReplyCallback(ar, () => { }, true), command);
+            MessageBus.RemoveSubscriber(this);
         }
 
-        private void SetWorkItemWaiting()
+        protected void SetWorkItemWaiting()
         {
             if (WorkItem == null)
             {
@@ -34,93 +38,9 @@ namespace Shuttle.Abacus.UI.Core.WorkItem
             WorkItem.Waiting();
         }
 
-        protected void Send(IMessage command, Action action)
+        protected void Send(object command)
         {
-            SetWorkItemWaiting();
-
-            Bus.Send(command).Register(ar => ReplyCallback(ar, action, true), command);
-        }
-
-        protected void SendNoComplete(IMessage command)
-        {
-            SetWorkItemWaiting();
-
-            Bus.Send(command).Register(ar => ReplyCallback(ar, () => { }, false), command);
-        }
-
-        protected void SendNoComplete(IMessage command, Action action)
-        {
-            SetWorkItemWaiting();
-
-            Bus.Send(command).Register(ar => ReplyCallback(ar, action, false), command);
-        }
-
-        protected void SendNoCallback(IMessage command)
-        {
-            Bus.Send(command);
-        }
-
-        private void ReplyCallback(IAsyncResult ar, Action action, bool complete)
-        {
-            if (WorkItem!= null)
-            {
-                WorkItem.Ready();
-            }
-
-            var result = ar.AsyncState as CompletionResult;
-
-            if (result == null)
-            {
-                return;
-            }
-
-            if (result.Messages == null)
-            {
-                return;
-            }
-
-            if (result.Messages.Length == 0)
-            {
-                return;
-            }
-
-            if (result.State == null)
-            {
-                return;
-            }
-
-            var reply = result.Messages[0] as ReplyMessage;
-
-            if (reply == null )
-            {
-                return;
-            }
-
-            if (reply.Result.HasMessages)
-            {
-                MessageBus.Publish(reply.Result);
-            }
-
-            if (!reply.Result.OK)
-            {
-                return;
-            }
-
-            action.Invoke();
-
-            if (!complete || WorkItem == null)
-            {
-                return;
-            }
-
-            MessageBus.Publish(new RefreshWorkItemDispatcherMessage(WorkItem));
-
-            MessageBus.Publish(new WorkItemCompletedMessage(WorkItem));
-        }
-
-        public void Dispose()
-        {
-            MessageBus.RemoveSubscriber(this);
+            ServiceBus.Send(command);
         }
     }
 }
