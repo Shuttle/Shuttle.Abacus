cd40edda397952f0e81e8094479890beaff6a6de
Eben <me@ebenroux.co.za>
2017-05-07 18:59:17 +0200

    - major pruning

--- Shuttle.Abacus.UI/Messages/Formula/DeleteFormulaMessage.cs
+++ Shuttle.Abacus.UI/Messages/Formula/DeleteFormulaMessage.cs
@@ -7,24 +7,21 @@ namespace Shuttle.Abacus.UI.Messages.Formula
 {
     public class DeleteFormulaMessage : Message
     {
-        public DeleteFormulaMessage(Resource ownerResource, string text, string ownerName, Guid ownerId, Guid formulaId)
+        public DeleteFormulaMessage(string text, Guid formulaId, Resource ownerResource)
         {
-            OwnerResource = ownerResource;
             Text = text;
-            OwnerName = ownerName;
-            OwnerId = ownerId;
             FormulaId = formulaId;
+            OwnerResource = ownerResource;
         }
 
-        public Resource OwnerResource { get; private set; }
         public string Text { get; private set; }
-        public string OwnerName { get; private set; }
-        public Guid OwnerId { get; private set; }
         public Guid FormulaId { get; private set; }
+
+        public Resource OwnerResource { get; private set; }
 
         public override IPermission RequiredPermission
         {
-            get { return Permissions.Method; }
+            get { return Permissions.Formula; }
         }
     }
 }
