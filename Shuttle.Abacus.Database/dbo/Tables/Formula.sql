7620c34254364b32d9f46325e15cf5493c260c1d
Eben <me@ebenroux.co.za>
2017-05-10 13:54:03 +0200

    - formula.sql

--- Shuttle.Abacus.Database/dbo/Tables/Formula.sql
+++ Shuttle.Abacus.Database/dbo/Tables/Formula.sql
@@ -3,7 +3,6 @@
     [Name]               VARCHAR (120)    NOT NULL,
     [MaximumFormulaName] VARCHAR (120)    CONSTRAINT [DF_Formula_MaximumFormulaName] DEFAULT ('') NOT NULL,
     [MinimumFormulaName] VARCHAR (120)    CONSTRAINT [DF_Formula_MinimumFormulaName] DEFAULT ('') NOT NULL,
-    [ExecutionType]      VARCHAR (25)     CONSTRAINT [DF_Formula_ExecutionType] DEFAULT ('First') NOT NULL,
     CONSTRAINT [PK_Formula] PRIMARY KEY NONCLUSTERED ([FormulaId] ASC)
 );
 
